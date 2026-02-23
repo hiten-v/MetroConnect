52bba4f6ffd8ea4585d1b849528f416c8297629a
Hiten Vaid <hitenvaid3837@gmail.com>
2026-02-23 05:36:07 +0530

    solved cors problem

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: [
+    'http://localhost:5173',
+    'https://metro-connect-hiten.vercel.app'
+ ],
+  credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
